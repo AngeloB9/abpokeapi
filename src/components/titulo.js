1a705208f7ebd58015e84800cc3d90e179810e51
Angelo Benavidez <benavidezangelo9@gmail.com>
2020-04-26 19:08:21 -0500

    Probando el reto de PokeApi

---
+++ src/components/titulo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Title extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>
+                    Angelo Benavidez
+                </h1>
+                <p>Hola JuanPa, este es el resultado del reto #1 de la unidad 2. Saludos</p>
+            </div>
+
+        ) 
+
+    }
+}

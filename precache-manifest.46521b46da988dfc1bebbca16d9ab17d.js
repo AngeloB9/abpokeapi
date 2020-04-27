79e7278b776bf2bee978c63441c1d8366cba3929
Angelo Benavidez <benavidezangelo9@gmail.com>
2020-04-26 20:58:22 -0500

    Updates

---
+++ precache-manifest.46521b46da988dfc1bebbca16d9ab17d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37a1e34088142186f88f2ecd71a5b24",
+    "url": "/abpokeapi/index.html"
+  },
+  {
+    "revision": "85d62627d5fc70345861",
+    "url": "/abpokeapi/static/css/main.5f2ada8f.chunk.css"
+  },
+  {
+    "revision": "c1cff0d3c7627269a64e",
+    "url": "/abpokeapi/static/js/2.5ed7cee7.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/abpokeapi/static/js/2.5ed7cee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d62627d5fc70345861",
+    "url": "/abpokeapi/static/js/main.228ea66f.chunk.js"
+  },
+  {
+    "revision": "234205d7c6a316f2f818",
+    "url": "/abpokeapi/static/js/runtime-main.b2b41ca8.js"
+  }
+]);
